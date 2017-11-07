bbe808583ea71a44b1bf630bcbb6195def5ab3e5
Shing Lyu <shing.lyu@booking.com>
2017-11-07 14:54:33 +0100

    Cleanup data/block.js

--- data/block.js
+++ data/block.js
@@ -1,4 +1,1 @@
-//window.location.replace('about:blank')
 window.location = "about:blank"
-//Chaning the innerHTML won't stop FB from sending notification!
-//document.body.innerHTML = '<h1 style="text-align:center;font-size:15vw;">Get back to work!</h1>';
